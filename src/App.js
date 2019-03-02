65972fbee7fd0ae406b15c3782d4d4365663307d
William Lorey <wwlorey@gmail.com>
2019-03-02 12:30:25 -0600

    Add sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { SimpleMap } from './Map.js';
+import { FoodMap } from './Map.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <SimpleMap />
+        <FoodMap />
+        <div className="SideBar">
+          Insert free food here !!!
+        </div>
       </div>
     );
   }
--- src/Map.js
+++ src/Map.js
@@ -1,34 +1,32 @@
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
- 
+import Map from 'google-map-react';
+
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
-export class SimpleMap extends Component {
+
+export class FoodMap extends Component {
   static defaultProps = {
     center: {
       // S&T's lat/lng
       lat: 37.954825,
-      lng: -91.773491
+      lng: -91.773491,
     },
-    zoom: 17.5
+    zoom: 17.5,
   };
- 
+
   render() {
     return (
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
+      <div className="Map">
+        <Map
           bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_KEY }}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-
           <AnyReactComponent
             lat={37.954825}
             lng={-91.773491}
             text={'Serving S&T since 8pm yesterday.'}
           />
-
-        </GoogleMapReact>
+        </Map>
       </div>
     );
   }
